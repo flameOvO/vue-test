ca7db10794038106bd8fc780f37929a3255eba0c
行冥 <jf@xingmingdeMacBook-Pro.local>
2020-01-08 08:13:56 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 v-if="count % 2 === 0">{{title}}</h1>
+    <h2>{{msg}}</h2>
+    <h3>{{combine}}</h3>
+    <button @click="add">add</button>
+    <input v-model="msg"/> 
+    <HelloWorld :msg="msg">
+      <!-- {{user}} -->
+      <template  v-slot="user">
+        <div>{{user.innerMsg}}</div>
+        <div>这是一个slot</div>
+      </template>
+    </HelloWorld>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  props: ['title'],
   components: {
     HelloWorld
+  },
+  watch: {
+      msg(newVal, oldVal){
+          if(newVal !== oldVal){
+            document.title=newVal;
+          }
+      }
+  },
+  data() {
+    return{
+        msg: 'Vue',
+        count: 0
+    };
+  },
+  computed: {
+    combine(){
+      return this.title + this.msg;
+    }
+  },
+  methods: {
+    add(){
+        return this.msg + this.count;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
